62612bee60869ac91179b7a8474529d8d1bc3cfe
Eche Arista <123518813+EcheArista@users.noreply.github.com>
2025-05-29 23:12:42 -0600

    Update db.js

--- db.js
+++ db.js
@@ -2,13 +2,16 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'administrador1234',
-  database: process.env.DB_NAME || 'usuarios',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: false, // IMPORTANTE para Railway
+  connectTimeout: 10000 // 10 segundos de tolerancia
 });
 
 module.exports = pool;
